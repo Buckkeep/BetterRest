ca55a8cecd56bf4914e0293e4be773e9efa2c7c0
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-08 15:44:44 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
